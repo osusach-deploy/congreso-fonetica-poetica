f8f286e4bf79be30971df1da89332a7227fc2054
Skyrdow <lucas.mesias@usach.cl>
2024-06-18 17:57:44 -0400

    add: content

--- src/components/Dropdown.svelte
+++ src/components/Dropdown.svelte
@@ -2,26 +2,12 @@
   export let i18n;
   export let currentLocale;
   export let pathname;
-  let pathTo = "";
   let selectedLanguage = 'Español';
   let showDropdown = false;
-
-  console.log("/" + pathname.split("/"));
-  console.log("/en" + pathname);
+  
+  let pathTo = "/" + (pathname.split("/").pop() ?? "")
 
   function changeLanguage(currentLocale) {
-      if (currentLocale == 'it'){
-        selectedLanguage = "Italiano"
-      }
-      if (currentLocale == 'es'){
-        selectedLanguage = "Español"
-      }
-      if (currentLocale == 'en'){
-        selectedLanguage = "English"
-      }
-  }
-
-  function changePrefix(currentLocale) {
       if (currentLocale == 'it'){
         selectedLanguage = "Italiano"
       }
@@ -46,9 +32,9 @@
         <p class="text-white text-base sm:text-lg bg-white bg-opacity-15 rounded-lg px-3 py-1">{ i18n.language }: <b>{selectedLanguage}</b></p>
     </button>
     <div class="dropdown-content top-16 right-0.5 rounded-lg {showDropdown ? 'show' : ''}">
-        <a href={"/" + pathname} class="rounded-lg hover:bg-slate-100">Español</a>
-        <a href="/en" class="rounded-lg hover:bg-slate-100">English</a>
-        <a href="/it" class="rounded-lg hover:bg-slate-100">Italiano</a>
+        <a href={pathTo} class="rounded-lg hover:bg-slate-100">Español</a>
+        <a href={"/en" + pathTo} class="rounded-lg hover:bg-slate-100">English</a>
+        <a href={"/it" + pathTo} class="rounded-lg hover:bg-slate-100">Italiano</a>
     </div>
 </div>
   
--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -4,23 +4,24 @@
   export let currentLocale;
   export let pathname;
 
-  let langPath = currentLocale == "es" ? "" : "/"+currentLocale;
-  console.log(langPath);
+  let langPath = currentLocale == "es" ? "" : "/" + currentLocale;
 </script>
 
 <div
   class="flex flex-row flex-wrap justify-between gap-y-4 bg-slate-700 w-full py-2 px-5"
 >
   <img src="/img/UsachSB.avif" alt="usachlogo" class="h-20" />
-  <Dropdown {i18n} {currentLocale} {pathname}/>
+  <Dropdown {i18n} {currentLocale} {pathname} />
 </div>
 
 <div
   class="flex flex-row flex-wrap justify-between bg-slate-700 w-full gap-y-4 py-4 px-5 text-white"
 >
-  <a class="hover:underline" href="/">Congreso</a>
-  <a class="hover:underline" href="/group">Grupo de investigación</a>
-  <a class="hover:underline" href="/comitees">Comités</a>
-  <a class="hover:underline" href="/#form">Convocatoria</a>
-  <a class="hover:underline" href="/proposals">Propuestas</a>
+  <a class="hover:underline" href={langPath + "/"}>Congreso</a>
+  <a class="hover:underline" href={langPath + "/group"}
+    >Grupo de investigación</a
+  >
+  <a class="hover:underline" href={langPath + "/comitees"}>Comités</a>
+  <a class="hover:underline" href={langPath + "/#form"}>Convocatoria</a>
+  <a class="hover:underline" href={langPath + "/proposals"}>Propuestas</a>
 </div>
