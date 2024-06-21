8046545963ec2c1697b90d1eb2ee115d08a2b2a3
Skyrdow <lucas.mesias@usach.cl>
2024-06-20 22:18:42 -0400

    add: prettier config

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -8,6 +8,16 @@ export default {
         tabWidth: 2,
         useTabs: false,
         parser: "astro",
+        bracketSameLine: true,
+      },
+    },
+    {
+      files: "*.svelte",
+      options: {
+        tabWidth: 2,
+        useTabs: false,
+        parser: "svelte",
+        bracketSameLine: true,
       },
     },
   ],
