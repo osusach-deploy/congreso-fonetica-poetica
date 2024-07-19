5cde40df75870a49e2d2042ceca6e5943182744b
Don Uldaricio <nicolas.aguilera.g@usach.cl>
2024-07-19 16:19:34 -0400

    fix: format

--- src/components/DynamicInput.svelte
+++ src/components/DynamicInput.svelte
@@ -51,27 +51,31 @@
               fill="black"
               d="M7 21q-.825 0-1.412-.587T5 19V6H4V4h5V3h6v1h5v2h-1v13q0 .825-.587 1.413T17 21zm2-4h2V8H9zm4 0h2V8h-2z" /></svg>
         </button>
-      {:else}
-        {#if inputs.length < maxInputs}
-          <button
-            type="button"
-            for={label + index}
-            aria-label={"add a " + label + " field"}
-            on:click={addInput}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="26px"
-              height="26px"
-              viewBox="0 0 24 24"
-              ><path
-                fill="green"
-                d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10s10-4.477 10-10S17.523 2 12 2m5 11h-4v4h-2v-4H7v-2h4V7h2v4h4z" /></svg>
-          </button>
-        {:else if inputs.length == maxInputs}
-        <svg xmlns="http://www.w3.org/2000/svg" width="26px" height="26px" viewBox="0 0 1200 1200">
-          <path fill="green" d="m1004.237 99.152l-611.44 611.441l-198.305-198.305L0 706.779l198.305 198.306l195.762 195.763L588.56 906.355L1200 294.916z" />
+      {:else if inputs.length < maxInputs}
+        <button
+          type="button"
+          for={label + index}
+          aria-label={"add a " + label + " field"}
+          on:click={addInput}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="26px"
+            height="26px"
+            viewBox="0 0 24 24"
+            ><path
+              fill="green"
+              d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10s10-4.477 10-10S17.523 2 12 2m5 11h-4v4h-2v-4H7v-2h4V7h2v4h4z" /></svg>
+        </button>
+      {:else if inputs.length == maxInputs}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="26px"
+          height="26px"
+          viewBox="0 0 1200 1200">
+          <path
+            fill="green"
+            d="m1004.237 99.152l-611.44 611.441l-198.305-198.305L0 706.779l198.305 198.306l195.762 195.763L588.56 906.355L1200 294.916z" />
         </svg>
-        {/if}
       {/if}
     </div>
   {/each}
