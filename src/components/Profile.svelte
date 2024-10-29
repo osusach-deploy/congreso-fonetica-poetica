d137f1863c16228bcf36a19ee871d361abe8221e
Nicolás Aguilera <nicolas.aguilera.g@usach.cl>
2024-10-29 18:19:09 -0300

    add: Profile component, added to Group

---
+++ src/components/Profile.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let name;
+    export let institution;
+    export let img;
+    export let credit = "";
+</script>
+
+<div class="flex flex-col text-center">
+    <img class="w-full h-60 object-cover rounded-lg mb-3" src={img} alt={name}>
+    <p class=" font-medium text-2xl">{name}</p>
+    <p>{institution}</p>
+    <p class="text-base">{credit}</p>
+</div>
