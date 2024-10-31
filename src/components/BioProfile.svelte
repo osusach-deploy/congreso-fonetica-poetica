ef810f316f3ab7c35315636200e74107ba2be02f
Skyrdow <lucas.mesias@usach.cl>
2024-10-30 22:39:04 -0300

    add: foto faltante + divider

--- src/components/BioProfile.svelte
+++ src/components/BioProfile.svelte
@@ -5,6 +5,7 @@
 </script>
 
 <div class="flex flex-col">
+  <hr class="my-2"/>
   <img class="h-60 object-contain" src={img} alt={name} />
   <p class="text-2xl font-semibold">{name}</p>
   <p class="text-lg">{description}</p>
